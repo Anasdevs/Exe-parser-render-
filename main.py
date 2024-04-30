64a86f28fc23f369fbc767d1a7c95929c3785090
Anas saifi <68700511+Anasdevs@users.noreply.github.com>
2024-04-30 20:26:46 +0530

    changes

--- main.py
+++ main.py
@@ -67,4 +67,5 @@ def analyze():
         return jsonify({"Error": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # Run the app and listen on all network interfaces (0.0.0.0)
+    app.run(debug=True, host='0.0.0.0', port=5000)
